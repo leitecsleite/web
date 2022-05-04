c5c8c86cd5d30d73af55251792ddefb933a1d27d
Lucas Sousa <lucas@hibrido.com.br>
2022-05-03 21:47:29 -0300

    Feat: FeedBckSucessStep, adicionado para renderizar na tela quando for true

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -5,6 +5,7 @@ import thoughtImageUrl from '../../assets/img/Thought.svg';
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 
 export const feedbackTypes = {
@@ -38,23 +39,39 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
     const [feedback, setFeedback] = useState<FeedbackType | null>(null);
+    const [feedbackSent, setFeedbackSent] = useState(false);
 
-    function handleRestartFeedback(){
+    function handleRestartFeedback() {
+        setFeedbackSent(false); 
         setFeedback(null)
     }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            {!feedback ? (
-              <FeedbackTypeStep 
-                 onFeedbackTypeChanged ={setFeedback}
-              />
+
+            {feedbackSent ? (
+                <FeedbackSuccessStep 
+                onFeedbackRestartRequested={handleRestartFeedback}
+                />
             ) : (
-                <FeedbackContentStep
-                 feedbackType ={feedback}
-                 onFeedbackRestartRequested={handleRestartFeedback}
-                 />
-             )}
+                <>
+
+                    {!feedback ? (
+                        <FeedbackTypeStep
+                            onFeedbackTypeChanged={setFeedback}
+                        />
+                    ) : (
+                        <FeedbackContentStep
+                            feedbackType={feedback}
+                            onFeedbackRestartRequested={handleRestartFeedback}
+                            onFeedbackSent={() => setFeedbackSent(true)}
+                        />
+                    )}
+                </>
+            )}
+
+
+
 
             <footer className='text-xs text-neutral-400'>
                 Feito com o amor <a href="" className='underline underline-offset-2'> Lucas Leite </a>
